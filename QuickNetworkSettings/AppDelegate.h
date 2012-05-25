4db84ee15e382b0d578cd504598cf3e11c901f72
Alec Geatches <throwpi@me.com>
2012-05-24 20:46:34 -0600

    Initial Commit

---
+++ QuickNetworkSettings/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QuickNetworkSettings
+//
+//  Created by Alec Geatches on 5/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QuickNetworkSettings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuickNetworkSettings
+//
+//  Created by Alec Geatches on 5/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
